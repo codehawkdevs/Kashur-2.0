5698394342868f8a059b56e04551fdc64caaa01f
Harsh Sahu <x3llsis@Harshs-MacBook-Air.local>
2019-06-14 00:16:40 +0530

    encryption, userDBsync added

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bryptjs");
 const { check, validationResult } = require("express-validator/check");
+
+const User = require("../../models/User");
 
 // @route   GET api/users
 // @desc    Register User
@@ -17,12 +21,45 @@ router.post(
       "Please enter a password with 8 or more characters"
     ).isLength({ min: 8 })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send("User Route");
+
+    const { name, email, password } = req.body;
+    try {
+      // See if user exists
+      let user = await User.findOne({ email });
+
+      if (user) {
+        res.status(400).json({ errors: [{ msg: "User already exists" }] });
+      }
+
+      // Get users gravatar
+      const avatar = gravatar.url(email, {
+        s: "200", //default size
+        r: "pg", // rating
+        d: "mm" // for defaut icon
+      });
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      });
+      // Encrypt password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save(); // saves to DB
+
+      // Return JsonWebToken
+      res.send("User Route");
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
   }
 );
 
